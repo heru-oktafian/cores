564e92ddf183531e19a47b6a7fd7fb83106b6ad1
Heru Oktafian <heru.oktafian@yahoo.com>
2026-01-23 04:29:30 +0700

    feat: add JWT and role-based middleware for authentication and authorization
    feat: create branch model with various attributes including subscription type and journal method
    
    feat: implement standard response models for API responses
    
    feat: define user model with roles and password hashing functionality
    
    feat: add user-branch relationship model for managing user access to branches
    
    feat: set up routing for authentication and user-related endpoints
    
    feat: implement database seeding for branches and users
    
    feat: create services for handling database operations, Google Drive interactions, and file management

--- configs/database_config.go
+++ configs/database_config.go
@@ -7,7 +7,7 @@ import (
 	strconv "strconv"
 	time "time"
 
-	// models "github.com/heru-oktafian/cores/models"
+	"github.com/heru-oktafian/cores/models"
 	redis "github.com/redis/go-redis/v9"
 	postgres "gorm.io/driver/postgres"
 	gorm "gorm.io/gorm"
@@ -51,39 +51,9 @@ func SetupDB() (err error) {
 	})
 
 	DB.AutoMigrate(
-	// &models.AnotherIncomes{},
-	// &models.BalanceReport{},
-	// &models.Branch{},
-	// &models.BuyReturnItems{},
-	// &models.BuyReturns{},
-	// &models.DailyProfitReport{},
-	// &models.DailyAsset{},
-	// &models.Defectas{},
-	// &models.DefectaItems{},
-	// &models.DuplicateReceiptItems{},
-	// &models.DuplicateReceipts{},
-	// &models.Expenses{},
-	// &models.FirstStockItems{},
-	// &models.FirstStocks{},
-	// &models.MemberCategory{},
-	// &models.Member{},
-	// &models.OpnameItems{},
-	// &models.Opnames{},
-	// &models.ProductCategory{},
-	// &models.Product{},
-	// &models.PurchaseItems{},
-	// &models.Purchases{},
-	// &models.Defectas{},
-	// &models.DefectaItems{},
-	// &models.SaleItems{},
-	// &models.Sales{},
-	// &models.SupplierCategory{},
-	// &models.Supplier{},
-	// &models.TransactionReports{},
-	// &models.UnitConversion{},
-	// &models.Unit{},
-	// &models.UserBranch{},
-	// &models.User{},
+		&models.User{},
+		&models.Branch{},
+		&models.UserBranch{},
 	)
 
 	// Check connection Postgres
@@ -91,7 +61,7 @@ func SetupDB() (err error) {
 		log.Fatalf("failed to connect to PostgreSQL database: %v", err)
 	}
 
-	// Panggil fungsi migrasi
+	// Panggil fungsi migrasi jika ada perubahan atau penambahan kolom
 	// err = runMigrations(DB)
 	// if err != nil {
 	// 	panic("failed to migrate: " + err.Error())
@@ -115,7 +85,7 @@ func SetupDB() (err error) {
 
 // runMigrations bertanggung jawab untuk menambahkan kolom baru
 func runMigrations(db *gorm.DB) error {
-	//m := db.Migrator()
+	// m := db.Migrator()
 
 	// Tambahkan kolom 'subscription_type' jika belum ada
 	// if !m.HasColumn(&models.Branch{}, "SubscriptionType") {
@@ -126,28 +96,6 @@ func runMigrations(db *gorm.DB) error {
 	// 	fmt.Println("Column 'subscription_type' successfully added.")
 	// } else {
 	// 	fmt.Println("Column 'subscription_type' already exists. Skipping.")
-	// }
-
-	// // Tambahkan kolom 'quota' jika belum ada
-	// if !m.HasColumn(&models.Branch{}, "Quota") {
-	// 	err := m.AddColumn(&models.Branch{}, "Quota")
-	// 	if err != nil {
-	// 		return fmt.Errorf("failed to add column 'quota': %w", err)
-	// 	}
-	// 	fmt.Println("Column 'quota' successfully added.")
-	// } else {
-	// 	fmt.Println("Column 'quota' already exists. Skipping.")
-	// }
-
-	// //Tambahkan kolom 'expired_date' Opname Item jika belum ada
-	// if !m.HasColumn(&models.FirstStockItems{}, "ExpiredDate") {
-	// 	err := m.AddColumn(&models.FirstStockItems{}, "ExpiredDate")
-	// 	if err != nil {
-	// 		return fmt.Errorf("failed to add column 'expired_date': %w", err)
-	// 	}
-	// 	fmt.Println("Column 'expired_date' Opname Items successfully added.")
-	// } else {
-	// 	fmt.Println("Column 'expired_date' Opname Items already exists. Skipping.")
 	// }
 
 	return nil
